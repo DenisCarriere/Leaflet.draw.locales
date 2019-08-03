155c23af97769cc0134b29f3cdcfec5e96a81ecc
Denis Carriere <carriere.denis@gmail.com>
2019-08-03 10:52:38 +0200

    update language locales code

--- src/index.ts
+++ src/index.ts
@@ -29,59 +29,64 @@ export const languages: Language[] = ["en", "fr", "es", "ru", "cs", "uk", "pt", 
  */
 export const drawLocales = (language: Language): DrawLocal => {
   let locale = en;
-  switch (language) {
+
+  switch (language.toLocaleLowerCase()) {
     case "en":
-    case "en_us":
-    case "en_us.utf-8":
+    case "en-us":
+    case "en-ca":
+    case "en-gb":
+    case "en-us.utf-8":
     case "english": {
       locale = en;
       break;
     }
     case "fr":
-    case "fr_us":
-    case "fr_us.utf-8":
+    case "fr-us":
+    case "fr-ca":
+    case "fr-us.utf-8":
     case "french": {
       locale = fr;
       break;
     }
     case "es":
-    case "es_us":
-    case "es_us.utf-8":
+    case "es-us":
+    case "es-ca":
+    case "es-us.utf-8":
     case "spanish": {
       locale = es;
       break;
     }
     case "sk":
-    case "sk_sk":
-    case "sk_sk.utf-8":
+    case "sk-sk":
+    case "sk-sk.utf-8":
     case "slovak": {
       locale = sk;
       break;
     }
     case "pt":
-    case "pt_pt":
-    case "pt_pt.utf-8":
+    case "pt-pt":
+    case "pt-pt.utf-8":
     case "portuguese": {
       locale = pt;
       break;
     }
     case "uk":
-    case "uk_ua":
-    case "uk_ua.utf-8":
+    case "uk-ua":
+    case "uk-ua.utf-8":
     case "ukrainian": {
       locale = uk;
       break;
     }
     case "cs":
-    case "cs_cz":
-    case "cs_cz.utf-8":
+    case "cs-cz":
+    case "cs-cz.utf-8":
     case "czech": {
       locale = cs;
       break;
     }
     case "ru":
-    case "ru_ru":
-    case "ru_ru.utf-8":
+    case "ru-ru":
+    case "ru-ru.utf-8":
     case "russian": {
       locale = ru;
       break;
@@ -115,14 +120,14 @@ declare class L {
 }
 
 export type Language =
-  | "en" | "en_us" | "en_us.utf-8" | "english"
-  | "fr" | "fr_us" | "fr_us.utf-8" | "french"
-  | "es" | "es_us" | "es_us.utf-8" | "spanish"
-  | "sk" | "sk_sk" | "sk_sk.utf-8" | "slovak"
-  | "pt" | "pt_pt" | "pt_pt.utf-8" | "portuguese"
-  | "uk" | "uk_ua" | "uk_ua.utf-8" | "ukrainian"
-  | "cs" | "cs_cz" | "cs_cz.utf-8" | "czech"
-  | "ru" | "ru_ru" | "ru_ru.utf-8" | "russian"
+  | "en" | "en-us" | "en-us.utf-8" | "english" | "en-ca" | "en-gb"
+  | "fr" | "fr-us" | "fr-us.utf-8" | "french" | "fr-ca"
+  | "es" | "es-us" | "es-us.utf-8" | "spanish" | "es-ca"
+  | "sk" | "sk-sk" | "sk-sk.utf-8" | "slovak"
+  | "pt" | "pt-pt" | "pt-pt.utf-8" | "portuguese"
+  | "uk" | "uk-ua" | "uk-ua.utf-8" | "ukrainian"
+  | "cs" | "cs-cz" | "cs-cz.utf-8" | "czech"
+  | "ru" | "ru-ru" | "ru-ru.utf-8" | "russian"
   | "nl" | "nl-nl" | "nl-nl.utf-8" | "dutch"
          | "nl-be" | "nl-be.utf-8" | "belgian" | "flemish";
 
